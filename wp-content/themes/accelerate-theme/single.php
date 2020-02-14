ba59c3e0ee21aa1484580642cee78ac7a4880cb9
ss-designs <design@suzistranahan.com>
2020-02-14 12:08:49 -0800

    Created contact page

--- wp-content/themes/accelerate-theme/single.php
+++ wp-content/themes/accelerate-theme/single.php
@@ -9,21 +9,21 @@
 
 get_header(); ?>
 
-<section class="single-page">
-	<div class="site-content">
-		<div class="main-content">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part('content', get_post_format()); ?>
-				<?php comments_template(); ?>
-			<?php endwhile; ?>
+	<section class="single-page">
+		<div class="site-content">
+			<div class="main-content">
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php get_template_part('content', get_post_format()); ?>
+					<?php comments_template(); ?>
+				<?php endwhile; ?>
+			</div>
+	
+			<?php get_sidebar(); ?>
 		</div>
+	</section>
 
-		<?php get_sidebar(); ?>
-	</div>
-</section>
-
-<nav id="navigation" class="container">
-	<div class="left"><a href="<?php echo site_url('/blog/') ?>">&larr; <span>Back to posts</span></a></div>
-</nav>
+	<nav id="navigation" class="container">
+		<div class="left"><a href="<?php echo site_url('home/') ?>">&larr; <span>Back to Work</span></a></div>
+	</nav>
 
 <?php get_footer(); ?>
